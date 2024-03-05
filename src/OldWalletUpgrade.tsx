5c45b097d7ff2a28f9c3ae36bd9644d664ac273d
Behrang Norouzinia <behrangn@gmail.com>
2024-03-05 12:49:46 +0330

    Change upgrade message

--- src/OldWalletUpgrade.tsx
+++ src/OldWalletUpgrade.tsx
@@ -16,12 +16,14 @@ const OldWalletUpgrade = observer(({ model }: Props) => {
             <div className='bg-attention dark:bg-attentiondark m-4 flex max-w-2xl flex-col items-center rounded-2xl p-4 shadow-sm'>
                 <h3 className='my-4 text-xl font-bold'>Upgrade to Hipo Version 2</h3>
                 <p className='max-w-xl px-4 py-2'>
-                    Press the &quot;Upgrade&quot; button below to automatically unstake from the old version and stake
-                    in the new version.
+                    Press &quot;Upgrade&quot; below to switch automatically from the old to the new version.
                 </p>
                 <p className='max-w-xl px-4 py-2'>
-                    You have <b>{model.oldWalletTokensFormatted}</b> in the old version. After upgrading, you&apos;ll
-                    receive <b>{model.newWalletTokensFormatted}</b> in the new version.
+                    You have <b>{model.oldWalletTokensFormatted}</b> in the old version. After the upgrade, you&apos;ll
+                    get <b>{model.newWalletTokensFormatted}</b> in the new version.
+                </p>
+                <p className='max-w-xl px-4 py-2'>
+                    After confirming, it may take a few minutes to receive the new hTON. Don&apos;t worry!
                 </p>
                 <button
                     className='my-4 rounded-2xl bg-orange px-16 py-2 text-lg font-medium text-white dark:text-dark-600'
