31a9b1b920d019f88210d209ff87b225e6617839
Behrang Norouzinia <behrangn@gmail.com>
2024-04-21 10:03:34 +0330

    Improve display of defi page

--- src/Defi.tsx
+++ src/Defi.tsx
@@ -16,15 +16,15 @@ const Referral = observer(({ model }: Props) => {
                 Maximize the potential of your capital with hTON in TON DeFi protocols
             </p>
 
-            <div className='m-8 mt-16 flex flex-col items-start gap-4 sm:flex-row'>
-                <div className='flex-1'>
+            <div className='m-8 mt-32 flex flex-col items-start gap-4 sm:flex-row'>
+                <div className='flex-1 sm:max-w-64'>
                     <h3 className='py-4 text-2xl font-bold'>Swap on DEXs</h3>
                     <p className='my-4 text-lg'>hTON can be traded on DEXs for other tokens.</p>
                 </div>
                 <div className='flex w-full flex-1 flex-col flex-wrap items-center justify-center gap-4 md:flex-row'>
-                    <div className='flex w-52 flex-none flex-col rounded-2xl border border-dark-600 border-opacity-50 bg-milky bg-opacity-50 p-4 text-center shadow-md dark:border-milky dark:border-opacity-50 dark:bg-dark-700'>
+                    <div className='flex w-44 flex-none flex-col rounded-2xl border border-dark-600 border-opacity-50 bg-milky bg-opacity-50 p-4 text-center shadow-md dark:border-milky dark:border-opacity-50 dark:bg-dark-700'>
                         <img src={dedust} className='mx-auto w-12' />
-                        <p className='m-4'>DeDust</p>
+                        <p className='m-4 font-medium'>DeDust</p>
                         <a
                             className='mx-4 rounded-xl bg-orange p-2 text-white dark:text-dark-600'
                             href={model.dedustSwapUrl}
@@ -33,9 +33,9 @@ const Referral = observer(({ model }: Props) => {
                             Swap now
                         </a>
                     </div>
-                    <div className='flex w-52 flex-none flex-col rounded-2xl border border-dark-600 border-opacity-50 bg-milky bg-opacity-50 p-4 text-center shadow-md dark:border-milky dark:border-opacity-50 dark:bg-dark-700'>
+                    <div className='flex w-44 flex-none flex-col rounded-2xl border border-dark-600 border-opacity-50 bg-milky bg-opacity-50 p-4 text-center shadow-md dark:border-milky dark:border-opacity-50 dark:bg-dark-700'>
                         <img src={ston} className='mx-auto w-12' />
-                        <p className='m-4'>StonFi</p>
+                        <p className='m-4 font-medium'>StonFi</p>
                         <a
                             className='mx-4 rounded-xl bg-orange p-2 text-white dark:text-dark-600'
                             href={model.stonSwapUrl}
@@ -47,15 +47,15 @@ const Referral = observer(({ model }: Props) => {
                 </div>
             </div>
 
-            <div className='m-8 mt-16 flex flex-col items-start gap-4 sm:flex-row'>
-                <div className='flex-1'>
+            <div className='m-8 mt-48 flex flex-col items-start gap-4 sm:flex-row'>
+                <div className='flex-1 sm:max-w-64'>
                     <h3 className='py-4 text-2xl font-bold'>Provide Liquidity</h3>
                     <p className='my-4 text-lg'>Use hTON to provide liquidity on DEXs, earning fees, and reward.</p>
                 </div>
                 <div className='flex w-full flex-1 flex-col flex-wrap items-center justify-center gap-4 md:flex-row'>
-                    <div className='flex w-52 flex-none flex-col rounded-2xl border border-dark-600 border-opacity-50 bg-milky bg-opacity-50 p-4 text-center shadow-md dark:border-milky dark:border-opacity-50 dark:bg-dark-700'>
+                    <div className='flex w-44 flex-none flex-col rounded-2xl border border-dark-600 border-opacity-50 bg-milky bg-opacity-50 p-4 text-center shadow-md dark:border-milky dark:border-opacity-50 dark:bg-dark-700'>
                         <img src={dedust} className='mx-auto w-12' />
-                        <p className='m-4'>DeDust</p>
+                        <p className='m-4 font-medium'>DeDust</p>
                         <a
                             className='mx-4 rounded-xl bg-orange p-2 text-white dark:text-dark-600'
                             href={model.dedustPoolUrl}
@@ -64,9 +64,9 @@ const Referral = observer(({ model }: Props) => {
                             Earn now
                         </a>
                     </div>
-                    <div className='flex w-52 flex-none flex-col rounded-2xl border border-dark-600 border-opacity-50 bg-milky bg-opacity-50 p-4 text-center shadow-md dark:border-milky dark:border-opacity-50 dark:bg-dark-700'>
+                    <div className='flex w-44 flex-none flex-col rounded-2xl border border-dark-600 border-opacity-50 bg-milky bg-opacity-50 p-4 text-center shadow-md dark:border-milky dark:border-opacity-50 dark:bg-dark-700'>
                         <img src={ston} className='mx-auto w-12' />
-                        <p className='m-4'>StonFi</p>
+                        <p className='m-4 font-medium'>StonFi</p>
                         <a
                             className='mx-4 rounded-xl bg-orange p-2 text-white dark:text-dark-600'
                             href={model.stonPoolUrl}
@@ -78,15 +78,15 @@ const Referral = observer(({ model }: Props) => {
                 </div>
             </div>
 
-            <div className='m-8 mt-16 flex flex-col items-start gap-4 sm:flex-row'>
-                <div className='flex-1'>
+            <div className='m-8 mt-48 flex flex-col items-start gap-4 sm:flex-row'>
+                <div className='flex-1 sm:max-w-64'>
                     <h3 className='py-4 text-2xl font-bold'>Take a Loan</h3>
                     <p className='my-4 text-lg'>Use hTON as collateral for a loan.</p>
                 </div>
                 <div className='flex w-full flex-1 flex-col flex-wrap items-center justify-center gap-4 md:flex-row'>
-                    <div className='flex w-52 flex-none flex-col rounded-2xl border border-dark-600 border-opacity-50 bg-milky bg-opacity-50 p-4 text-center opacity-50 shadow-md dark:border-milky dark:border-opacity-50 dark:bg-dark-700'>
+                    <div className='flex w-44 flex-none flex-col rounded-2xl border border-dark-600 border-opacity-50 bg-milky bg-opacity-50 p-4 text-center opacity-50 shadow-md dark:border-milky dark:border-opacity-50 dark:bg-dark-700'>
                         <img src={evaa} className='mx-auto w-12' />
-                        <p className='m-4'>Evaa</p>
+                        <p className='m-4 font-medium'>Evaa</p>
                         {/* <a
                             className='mx-4 rounded-xl bg-orange p-2 text-white dark:text-dark-600'
                             href={model.evaaLoanUrl}
