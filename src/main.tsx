304fd2edb5f992ecaa0407ef981d41bc3ab759c8
Behrang Norouzinia <behrangn@gmail.com>
2024-10-07 12:06:51 +0330

    Add a check for html frames

--- src/main.tsx
+++ src/main.tsx
@@ -5,11 +5,15 @@ import { Model } from './Model.ts'
 import '@twa-dev/sdk'
 import './index.css'
 
-const model = new Model()
-model.init()
+if (self === top) {
+    const model = new Model()
+    model.init()
 
-ReactDOM.createRoot(document.querySelector('#root') ?? document.body).render(
-    <React.StrictMode>
-        <App model={model} />
-    </React.StrictMode>,
-)
+    ReactDOM.createRoot(document.querySelector('#root') ?? document.body).render(
+        <React.StrictMode>
+            <App model={model} />
+        </React.StrictMode>,
+    )
+} else {
+    console.error('Inside a frame')
+}
