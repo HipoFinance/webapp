3fc3214ce298926591fae3dac389afa30724197a
Alireza Ravanbakhsh <alireza.ravanbakhsh@gmail.com>
2024-11-27 12:38:42 +0330

    Enhance statistics section

--- src/Stats.tsx
+++ src/Stats.tsx
@@ -38,7 +38,12 @@ const Stats = observer(({ model }: Props) => {
                         <p className='ml-auto'>{model.protocolFee}</p>
                     </div>
                     <div className='my-4 flex flex-row'>
-                        <p>Currently staked</p>
+                        <p>Staked</p>
+                        <img src={question} className='peer ml-1 w-4 dark:hidden' />
+                        <img src={questionDark} className='peer ml-1 hidden w-4 dark:block' />
+                        <p className='absolute left-1/3 top-6 z-10 hidden -translate-x-1/4 rounded-lg bg-lightblue p-4 text-xs font-normal text-blue shadow-xl peer-hover:block'>
+                            Total TON currently staked in Hipo.
+                        </p>
                         <p className='ml-auto'>{model.currentlyStaked}</p>
                     </div>
                     <div className='relative my-4 flex flex-row'>
