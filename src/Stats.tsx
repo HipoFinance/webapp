d351cd7c945babe09531a5a8a6f706aebaad2b64
Alireza Ravanbakhsh <alireza.ravanbakhsh@gmail.com>
2024-11-27 12:14:49 +0330

    Fix a title

--- src/Stats.tsx
+++ src/Stats.tsx
@@ -42,7 +42,7 @@ const Stats = observer(({ model }: Props) => {
                         <p className='ml-auto'>{model.currentlyStaked}</p>
                     </div>
                     <div className='relative my-4 flex flex-row'>
-                        <p>Holders Count</p>
+                        <p>Holders</p>
                         <img src={question} className='peer ml-1 w-4 dark:hidden' />
                         <img src={questionDark} className='peer ml-1 hidden w-4 dark:block' />
                         <p className='absolute left-1/3 top-6 z-10 hidden -translate-x-1/4 rounded-lg bg-lightblue p-4 text-xs font-normal text-blue shadow-xl peer-hover:block'>
