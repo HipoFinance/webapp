cb27fbea43f0ddc43215287e5cc4068605d8e017
Behrang Norouzinia <behrangn@gmail.com>
2026-05-02 15:23:42 +0330

    Remove warnings from build process

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import { nodePolyfills } from 'vite-plugin-node-polyfills'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react(), nodePolyfills()],
+    plugins: [react()],
+    define: {
+        global: 'globalThis',
+    },
 })
